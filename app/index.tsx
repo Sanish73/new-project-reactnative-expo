a86432eecf6cc7ca40ab23725a4bb0a081fd9e81
sanish73 <thapasanish73@gmail.com>
2026-02-15 00:31:46 +0545

    chnegs

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,83 @@
-import { Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import {
+  FlatList,
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
 
-export default function index() {
+export interface Offer {
+  id: number;
+  title: string;
+  image: ImageSourcePropType;
+  color: string;
+}
+
+export default function Index() {
+  const renderOffer = ({ item, index }: { item: Offer; index: number }) => {
+    const isEven = index % 2 === 0;
+    return (
+      <View>
+        <Pressable
+          className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+          style={{
+            backgroundColor: item.color,
+          }}
+        >
+          {({ pressed }) => (
+            <Fragment>
+              <View
+                className="h-full w-1/2"
+                style={{
+                  backgroundColor: item.color,
+                }}
+              >
+                <Image
+                  source={item.image}
+                  className="w-full h-full"
+                  resizeMode="contain"
+                />
+              </View>
+              <View className="offer-card__info">
+                <Text className="h1-bold text-white leading-tight ">
+                  {item.title}
+                </Text>
+                <Image
+                  source={images.arrowRight}
+                  className="size-10"
+                  resizeMode="contain"
+                  tintColor="#ffffff"
+                />
+              </View>
+            </Fragment>
+          )}
+        </Pressable>
+      </View>
+    );
+  };
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500 ">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={offers}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderOffer}
+        contentContainerClassName="pb-28 px-5"
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white", // ensures background covers the curved edges
+  },
+});
